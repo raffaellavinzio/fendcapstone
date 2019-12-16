00ab21670df1b9b260c46e0a8a62387827858136
Raffaella Vinzio <raffaella.vinzio@gmail.com>
2019-12-16 03:04:59 +0100

    removed dist from gitignore

---
+++ dist/precache-manifest.09415f701295e805288df932645deb7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6b026adafeae2ba2de8df22c3fbee19",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5a0a8bbc555db860b5b",
+    "url": "main.css"
+  },
+  {
+    "revision": "e5a0a8bbc555db860b5b",
+    "url": "main.js"
+  },
+  {
+    "revision": "13cc0648d57f8d9b81888ca039df522c",
+    "url": "media/darkskylogo.svg"
+  },
+  {
+    "revision": "d8ae3b04380558462cf1090cfa0b7828",
+    "url": "media/geonameslogo.svg"
+  },
+  {
+    "revision": "f7d4c16ca73aaeebb7fedf8b7332fcf6",
+    "url": "media/pixabaylogo.svg"
+  }
+]);
